ee6c4105fca0fe530e3a2fc13d7142144088887c
Paro1ev <147792972+Paro1ev@users.noreply.github.com>
2023-10-13 10:20:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1
+
+// let num = 55;
+// console.log("Число:", num);
+
+// if (num <0 ){
+//     console.log("Отрицательное")
+// } else {
+//     console.log("Положительное")
+// }
+
+
+// 2
+
+// let str="Я люблю разработку мобильных приложений";
+// console.log(str.length);
+
+
+// 3
+
+// let str = "Я очень люблю разработку мобильных приложений";
+// let str2 = str[str.length -1];
+// console.log(str2)
+
+// 4
+
+// let a = 20
+// if (a % 2 === 0) {
+//     console.log("Четное")    
+// }
+// else {
+//     console.log("Нечетное")
+// }
+
+// 5
+
+// let word1 = "Мама"
+// let word2 = "Папа"
+
+// if (word1[0] === word2[0]){
+//     console.log("Совпадают");
+// } else {
+//     console.log("Не совпадают");
+// }
+
+// 6
+
+// let num = 52345;
+// let first = num.toString()[0];
+// let last = num.toString().slice(-1);
+// let sum = +first + +last;
+// console.log("Сумма первой и последней цифры " + sum);
+
+// 7
+
+// let num = 1011;
+// let sum = 0;
+
+// while (num > 0){
+//     sum += num % 10;
+//     num = Math.floor(num / 10); 
+// }
+// console.log("Сумма цифр числа " + sum);
